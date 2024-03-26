1d961a6f3a6a6fba25e922dba9308c4a37414e3f
HEEMOON YOON <jcjc7890@gmail.com>
2024-03-26 15:05:29 +1100

    up to date dependencies

--- Undistort.py
+++ Undistort.py
@@ -1,7 +1,7 @@
 #Undisort.py
 #Created by Chris Rillahan
-#Last Updated: 02/04/2015
-#Written with Python 2.7.2, OpenCV 2.4.8 and NumPy 1.8.0
+#Last Updated: 26/03/2024
+#Written with Python 3.7.8, OpenCV 4.5.4 and NumPy 1.21.6
 
 #This program takes a video file and removes the camera distortion based on the
 #camera calibration parameters.  The filename and the calibration data filenames
@@ -18,9 +18,9 @@
 import numpy as np
 import cv2, time, sys
 
-filename = 'GOPR0028.MP4'
+filename = './input.mp4'
 
-print 'Loading data files'
+print('Loading data files')
 
 npz_calib_file = np.load('calibration_data.npz')
 
@@ -33,30 +33,32 @@ print('Finished loading files')
 print(' ')
 print('Starting to undistort the video....')
 
-#Opens the video import and sets parameters
+# Opens the video import and sets parameters
 video = cv2.VideoCapture(filename)
-#Checks to see if a the video was properly imported
+
+# Checks to see if a the video was properly imported
 status = video.isOpened()
 
 if status == True:
-    FPS = video.get(cv2.cv.CV_CAP_PROP_FPS)
-    width = video.get(cv2.cv.CV_CAP_PROP_FRAME_WIDTH)
-    height = video.get(cv2.cv.CV_CAP_PROP_FRAME_HEIGHT)
+    FPS = video.get(cv2.CAP_PROP_FPS)
+    width = video.get(cv2.CAP_PROP_FRAME_WIDTH)
+    height = video.get(cv2.CAP_PROP_FRAME_HEIGHT)
     size = (int(width), int(height))
-    total_frames = video.get(cv2.cv.CV_CAP_PROP_FRAME_COUNT)
+    total_frames = video.get(cv2.CAP_PROP_FRAME_COUNT)
     frame_lapse = (1/FPS)*1000
 
-    #Initializes the export video file
-    codec = cv2.cv.CV_FOURCC('D','I','V','X')
+    # Initializes the export video file
+    codec = cv2.VideoWriter_fourcc('D','I','V','X')
     video_out = cv2.VideoWriter(str(filename[:-4])+'_undistored.avi', codec, FPS, size, 1)
 
-    #Initializes the frame counter
+    # Initializes the frame counter
     current_frame = 0
-    start = time.clock()
+    start = time.perf_counter()
 
     while current_frame < total_frames:
         success, image = video.read()
-        current_frame = video.get(cv2.cv.CV_CAP_PROP_POS_FRAMES)
+        image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
+        current_frame = video.get(cv2.CAP_PROP_POS_FRAMES)
 
         dst = cv2.undistort(image, intrinsic_matrix, distCoeff, None)
     
@@ -64,11 +66,11 @@ if status == True:
     
     video.release()
     video_out.release()
-    duration = (time.clock()-float(start))/60
+    duration = (time.perf_counter()-float(start))/60
 
     print(' ')
     print('Finished undistorting the video')
-    print('This video took:' + str(duration) + ' minutes')
+    print('This video took:' + str(round(duration,2)) + ' minutes')
 else:
     print("Error: Video failed to load")
     sys.exit()
